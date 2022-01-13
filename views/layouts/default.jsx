a9cd667275a975d76f1210480f8b1a3081183173
Chris Bricker <csbricker8@gmail.com>
2022-01-12 21:11:01 -0700

    sending index and show data

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -2,17 +2,15 @@ const React = require('react');
 
 function Default(html) {
     return (
-        <html>
-            <head>
-                <title>Default</title>
-            </head>
-            <body>
-                <h1>HTML Rendered!</h1>
-                <div className="container">
-                    {html.children}
-                </div>
-            </body>
-        </html>
+      <html>
+      <head>
+        <title>{html.title || 'Default'}</title>
+      </head>
+      <body>
+        <h1>HTML Rendered!</h1>
+        {html.children}
+      </body>
+      </html>
     )
 }
 
