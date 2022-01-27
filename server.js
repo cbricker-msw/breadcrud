c1edf7d3c470ee82a2e8f118aa9b5cc820469020
Chris Bricker <csbricker8@gmail.com>
2022-01-26 20:07:01 -0700

    Catch up

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 // DEPENDENCIES
-const express = require('express');
+const express = require('express')
+const req = require('express/lib/request')
+const {get} = require('express/lib/response')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
-require('dotenv').config();
-const PORT = process.env.PORT;
-const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true, useUnifiedTopology: true},
+  () => { console.log('Connected to MongoDB: ', process.env.MONGO_URI)})
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
+  res.send('Welcome to an Awesome App about Breads!')
 })
   
 // Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
 
+  app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 // LISTEN
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
-});
+})
